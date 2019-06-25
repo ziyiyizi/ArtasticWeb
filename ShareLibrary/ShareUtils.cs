c23839150d41db261cea18b9c60fd1ab12b3a56e
ziyiyizi <xiajinlei@foxmail.com>
2019-06-25 17:16:13 +0800

    sharelibrary

---
+++ ShareLibrary/ShareUtils.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ShareLibrary
+{
+    public sealed class ShareUtils
+    {
+        public static bool IsNullOrEmpty(string sv)
+        {
+            return string.IsNullOrEmpty(sv) || sv.Equals("null");
+        }
+
+        public static DateTime Now()
+        {
+            return DateTime.Now;
+        }
+
+        public static string UUID()
+        {
+            return Guid.NewGuid().ToString("N");
+        }
+    }
+}
